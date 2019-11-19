d4a81407f6daf10df918f77be0a63c43eb9dd56a
Tom Roberts <44359621+ksqk34@users.noreply.github.com>
2019-11-19 15:12:09 +0000

    Updated GPS App to support maps API

--- src/GPS Prototype/clientside/index.js
+++ src/GPS Prototype/clientside/index.js
@@ -1,20 +1,165 @@
+
 /* eslint-disable */
 var width = 250;
 var height = 250;
 
-var const_offsetX = 100;
-var const_offsetY = 100;
-var personX = 0;
-var personY = 0;
+var personX_Offset = 0;
+var personY_Offset = 0;
 var originalLat = 0;
 var originalLong = 0;
+var currentLat = 0;
+var currentLong = 0;
 
-var meters_per_lat = 111319.39694466339;
-var meters_per_long = 64346.67617209433;
 var polls = 0;
+var zoomLevel = 20;
+
+var cameraCoords = { lat: 0, lng: 0 };
+var personCoords = { lat: 0, lng: 0 };
 
 
-var pixelsPerMeter = 40;
+var platform = new H.service.Platform({
+      'apikey': 'gHz9eB7Du7VFdBGn7Zt4KFjLxRZGxLF1nhRLy5J96ik'
+    });
+
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+var baseLayer = defaultLayers.raster.satellite.map;
+baseLayer.setMax(30);
+
+var map = new H.Map(
+      document.getElementById('mapContainer'),
+      baseLayer,
+      //defaultLayers.vector.normal.map,
+      {
+        zoom: zoomLevel,
+        center: cameraCoords
+      });
+
+
+
+// var svgMarkup = '<svg width="24" height="24" ' +
+//       'xmlns="http://www.w3.org/2000/svg">' +
+//       '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+//       'height="22" /><text x="12" y="18" font-size="12pt" ' +
+//       'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+//       'fill="white">H</text></svg>';
+
+var svgMarkup = '<svg width="10" height="10"> <circle cx="0" cy="0" r="10" fill="blue" /></svg>'
+
+
+var icon = new H.map.DomIcon(svgMarkup)
+var marker = new H.map.DomMarker(personCoords, {icon: icon});
+// var icon2 = new H.map.DomIcon(svgMarkup2)
+// var marker2 = new H.map.DomMarker(personCoords, {icon: icon2});
+//map.setMax(25);
+
+map.addObject(marker);
+//map.addObject(marker2);
+// Create an icon, an object holding the latitude and longitude, and a marker:
+//var icon = new H.map.Icon(svgMarkup)
+//var coords3 = {lat: 0, lng: 0}
+//var marker = new H.map.Marker(coords3, {icon: icon});
+//map.addObject(marker);
+window.addEventListener('resize', () => map.getViewPort().resize());
+
+UpdateCameraPosition();
+
+
+setInterval(
+      () => UpdateUserPosition(),
+      50
+    );
+
+
+
+async function UpdateCameraPosition() {
+      polls += 1;
+
+      const { coords } = await getCurrentPosition();
+      var { latitude, longitude } = coords;
+
+      console.log("Found! " + latitude + " " + longitude);
+
+      cameraCoords = {lat: latitude, lng: longitude}
+      
+      personCoords = {lat: latitude, lng: longitude}
+      document.getElementById('xPos').innerHTML = "Lat: " + personCoords.lat;
+      document.getElementById('yPos').innerHTML = "long: " + personCoords.lng;
+      document.getElementById('tests').innerHTML = "location updates: " + polls;
+      
+
+      map.setCenter(cameraCoords);
+
+      //var canvases = document.getElementsByTagName("canvas");
+      //console.log(canvases.length);
+      //var ctx = canvases[0].getContext("2d");
+      //console.log(canvases[0]);
+
+      //ctx.strokeRect(5, 5, 25, 15);
+      //ctx.scale(2, 2);
+      //ctx.strokeRect(5, 5, 25, 15);
+      //var canvas = document.getElementById("myCanvas");
+
+}
+async function UpdateUserPosition(){
+      polls += 1;
+
+      const { coords } = await getCurrentPosition();
+      var { latitude, longitude } = coords;
+      //latitude += polls * 0.00001;
+      personCoords = {lat: latitude, lng: longitude}
+      document.getElementById('xPos').innerHTML = "Lat: " + personCoords.lat;
+      document.getElementById('yPos').innerHTML = "long: " + personCoords.lng;
+      document.getElementById('tests').innerHTML = "location updates: " + polls;
+      marker.setGeometry(personCoords);
+      //marker2.setGeometry(personCoords);
+
+      
+      // var canvases = document.getElementsByClassName("canvas");
+
+      // var ctx = canvases[0].getContext("2d");
+      //ctx.scale(8, 8);
+      //ctx.strokeRect(5, 5, 25, 15);
+}
+
+function getCurrentPosition(options = {}) {
+      return new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      });
+  }
+
+  function Reset() {
+      personX_Offset = 0;
+      personY_Offset = 0;
+      originalLat = 0;
+      originalLong = 0;
+      polls = 0;
+      document.getElementById('xPos').innerHTML = 0;
+      document.getElementById('yPos').innerHTML = 0;
+      document.getElementById('tests').innerHTML = 0;
+      zoomLevel = 20;
+      map.setZoom(zoomLevel);
+      UpdateCameraPosition();
+      
+}
+
+function ZoomIn() {
+      zoomLevel += 1;
+      map.setZoom(zoomLevel);
+      
+}
+function ZoomOut() {
+      zoomLevel -= 1;
+      map.setZoom(zoomLevel);
+      
+}
+
+
+
+//update();
+
+//coords = await fetchCoordinates().then(console.log("Yes"););
+
 
 
 
@@ -36,93 +181,66 @@ var pixelsPerMeter = 40;
 
 
 
-function setup() {
-      height = screen.height - document.getElementById('Wrap').clientHeight - 7;
-      width = document.getElementById('Wrap').clientWidth;
+// function setup() {
+//       height = screen.height - document.getElementById('Wrap').clientHeight - 7;
+//       width = document.getElementById('Wrap').clientWidth;
 
-      canvas1 = createCanvas(width, height);
-      canvas1.parent("holder");
-      const_offsetX = width / 2;
-      const_offsetY = height / 2;
+//       canvas1 = createCanvas(width, height);
+//       canvas1.parent("holder");
+//       const_offsetX = width / 2;
+//       const_offsetY = height / 2;
 
-}
+// }
 
-function UpdatePosition(lat, long) {
-      polls += 1;
-      document.getElementById('xPos').innerHTML = "Lat: " + lat;
-      document.getElementById('yPos').innerHTML = "long: " + long;
-      document.getElementById('tests').innerHTML = "location updates: " + polls;
-
-      if (originalLat == 0) {
-            originalLat = lat;
-            originalLong = long;
-            personX = 0;
-            personY = 0;
-      }
-      if (originalLong == 0) {
-            originalLong = long;
-            originalLat = lat;
-            personX = 0;
-            personY = 0;
-      }
-      x_diff = lat - originalLat;
-      y_diff = long - originalLong;
-      personX = x_diff * meters_per_lat;
-      personY = y_diff * meters_per_long;
-}
-
-function drawLines() {
-      stroke(180);
-      strokeWeight(1);
-      //line(const_offsetX - personX, 0, const_offsetX - personX, height); // Central Vertical line
-      //line(0, const_offsetY + personY, width, const_offsetY + personY);
-      var xCount = width / pixelsPerMeter;
-      var yCount = height / pixelsPerMeter;
-
-      for (let i = 0; i < xCount; i += 1) {
-            line((i * pixelsPerMeter) - personX, 0, (i * pixelsPerMeter) - personX, height);
-      }
-      for (let i = 0; i < yCount; i += 1) {
-            line(0, (i * pixelsPerMeter) + personY, width, (i * pixelsPerMeter) + personY);
-      }
-      // for (let i = this.particles.length - 1; i >= 0; i -= 1) {
-      //       this.particles[i].set_h(value);
-      // }
-}
-
-function showPosition(position) {
-      UpdatePosition(position.coords.latitude, position.coords.longitude);
-}
-
-function draw() {
-      background(255);
-      drawLines();
+// async function showPosition(position) {
+//       UpdatePosition(position.coords.latitude, position.coords.longitude);
+// }
 
 
-      if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-      // if (mouseIsPressed) {
-      //       fill(0);
-      // } else {
-      //       fill(255);
-      // }
-      strokeWeight(1);
+// async function fetchCoordinates2(){
+//       try {
+//             const { coords } = await getCurrentPosition();
+//             const { latitude, longitude } = coords;
+    
+//             console.log("Found! " + latitude + " " + longitude);
 
-      fill(color(100, 100, 200))
-      ellipse(const_offsetX, const_offsetY, 5, 5);
 
-}
+            
+//             //console.log(currentLat + " " + currentLong)
+//             // Instantiate (and display) a map object:
+//             // var map = new H.Map(
+//             //   document.getElementById('mapContainer'),
+//             //   defaultLayers.raster.satellite.map,
+//             //   //defaultLayers.vector.normal.map,
+//             //   {
+//             //     zoom: 20,
+//             //     center: { lat: latitude, lng: longitude }
+//             //   });
+            
+//             //var circle = new H.map.Circle(center, radius, opt_options)
+            
+//                         // Define a variable holding SVG mark-up that defines an icon image:
+//             // var svgMarkup = '<svg width="24" height="24" ' +
+//             // 'xmlns="http://www.w3.org/2000/svg">' +
+//             // '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+//             // 'height="22" /><text x="12" y="18" font-size="12pt" ' +
+//             // 'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+//             // 'fill="white">H</text></svg>';
 
-function Reset() {
-      personX = 0;
-      personY = 0;
-      originalLat = 0;
-      originalLong = 0;
-      polls = 0;
-      document.getElementById('xPos').innerHTML = 0;
-      document.getElementById('yPos').innerHTML = 0;
-      document.getElementById('tests').innerHTML = 0;
-}
+//             // // Create an icon, an object holding the latitude and longitude, and a marker:
+//             // var icon = new H.map.Icon(svgMarkup)
+//             // var coords3 = {lat: latitude, lng: longitude}
+//             // var marker = new H.map.Marker(coords3, {icon: icon});
+
+//             // Add the marker to the map and center the map at the location of the marker:
+//             map.addObject(marker);
+//             map.setCenter(coords3);
+
+//             console.log("Found2! " + latitude + " " + longitude);
+//             // Handle coordinates
+//         } catch (error) {
+//             // Handle error
+//             console.error(error);
+//         }
+// }
+
