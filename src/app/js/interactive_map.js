93719c15e2ff491d4b3e902e804eb9a1649ab3a6
ksqk34 <thomas.roberts3@durham.ac.uk>
2020-01-12 22:53:41 +0000

    Added rendering shadows+materials

--- src/app/js/interactive_map.js
+++ src/app/js/interactive_map.js
@@ -1,51 +1,59 @@
-//import { Vector3, RGBA_ASTC_10x10_Format } from "./three";
+//var THREE = require('three');
+//import { OrbitControls } from '../js/OrbitControls.js';
+
 
 var container = document.getElementsByClassName("renderObject")[0];
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, container.scrollWidth / container.scrollHeight, 0.1, 1000);
+
+//## RENDERER
 
 var renderer = new THREE.WebGLRenderer();
-
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 renderer.setSize(container.scrollWidth, container.scrollHeight);
 
+//## MATERIALS
+var material_grey = new THREE.MeshPhongMaterial({ color: 0x606060 });
+var material_light = new THREE.MeshPhongMaterial({ color: 0x8f6f7a });
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshPhongMaterial();
+//## GEOMETRY AND MESHES
 
-var cube = new THREE.Mesh(geometry, material);
-var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+var cone_geometry = new THREE.ConeGeometry(1, 2, 15);
+var cone_mesh = new THREE.Mesh(cone_geometry, material_light);
+cone_mesh.castShadow = true;
+
+var floor_geometry = new THREE.PlaneGeometry(5, 5);
+var floor_mesh = new THREE.Mesh(floor_geometry, material_grey);
+floor_mesh.receiveShadow = true;
+
+//## LIGHTS
+
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1, 100);
+directionalLight.position.set(1, 1, 1);
+directionalLight.castShadow = true;
+
+
 var light = new THREE.AmbientLight(0x202020);
 
-scene.add(cube);
+//## CAMERA CONTROL
+var camera = new THREE.PerspectiveCamera(75, container.scrollWidth / container.scrollHeight, 0.1, 1000);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 100;
+controls.maxPolarAngle = Math.PI / 2.1;
+
+
+//## SCENE INITIALISATION
+scene.add(cone_mesh);
+scene.add(floor_mesh);
 scene.add(directionalLight);
 scene.add(light);
 
-
-scene.background = new THREE.Color(0xf0f0f0);
-
-
-
-function start() {
-    camera.position.z = 5;
-    directionalLight.position.set(1, 1, 1);
-
-
-    console.log("Started");
-}
-
-function animate() {
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-
-
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-
-
-}
 
 if (WEBGL.isWebGLAvailable()) {
     container.appendChild(renderer.domElement);
@@ -59,4 +67,42 @@ if (WEBGL.isWebGLAvailable()) {
     container.appendChild(warning);
 
 }
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+    camera.aspect = container.scrollWidth / container.scrollHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.scrollWidth, container.scrollHeight);
+}
+
+
+//####### WEBGL three.js code  \/ \/
+
+
+function start() {
+    scene.background = new THREE.Color(0xf0f0f0);
+    camera.position.set(0, 5, 5);
+    cone_mesh.position.set(0, 1, 0);
+    //cone_mesh.rotation.x = Math.PI;
+    floor_mesh.rotation.x = -Math.PI / 2;
+    controls.update();
+
+
+
+    console.log("Started");
+}
+
+function animate() {
+
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
+
+
+
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+
+
+}
+
 
