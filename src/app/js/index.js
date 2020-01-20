6f8ec79171c60013e83217a0c1de497f3a497d1b
Dervillay <james.gascoigneburns7@gmail.com>
2020-01-20 20:08:01 +0000

    Ask for location permission on mobile online

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,16 +1,28 @@
 function getLocation() {
-  if (navigator.geolocation) {
+  console.log(isMobile())
+  if (isMobile() && navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
+  } else if (!navigator.geolocation) {
     alert("Geolocation is not supported by this browser.");
   }
 }
 
 function showPosition(position) {
   alert("Latitude: " + position.coords.latitude + "    Longitude: " + position.coords.longitude);
-  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 || position.coords.longitude > -1.621184 || position.coords.longitude < -1.6256) {
+  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 || 
+      position.coords.longitude > -1.621184 || position.coords.longitude < -1.6256) {
     alert("you aint in Beaurepaire")
   }
+}
+
+// Return whether user is viewing site on mobile
+function isMobile() {
+  var width = Math.max(document.documentElement.clientWidth, window.innerWidth);
+
+  if (width < 600) {
+    return true;
+  }
+  return false;
 }
 
 // 54.789525, -1.623473
