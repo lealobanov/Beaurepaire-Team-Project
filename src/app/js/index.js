0ade7fe7f1218f82a865a7863bd88454ac2b9009
Dervillay <james.gascoigneburns7@gmail.com>
2020-01-20 21:52:08 +0000

    Change mobile information depending on location

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,18 +1,27 @@
 function getLocation() {
   if (isMobile()) {
     try {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(displayButtons);
     } catch (err) {
       alert("Error: " + err.message + ". Unable to obtain location information");
     }
   }
 }
 
-function showPosition(position) {
-  alert("Latitude: " + position.coords.latitude + "    Longitude: " + position.coords.longitude);
-  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 ||
-      position.coords.longitude > -1.621184 || position.coords.longitude < -1.6256) {
-    alert("you aint in Beaurepaire")
+// Display appropriate buttons depending on user location (coords will need improving)
+function displayButtons(position) {
+  // Hide request for location access
+  document.getElementById("info-text").style.display = "none";
+
+  // Display buttons
+  if (54.790737 < position.coords.latitude < 54.788654 &&
+      -1.621184 < position.coords.longitude < -1.6256) {
+    document.getElementById("header").innerHTML = "Welcome to Beaurepaire";
+    document.getElementById("explore-btn").style.display = "block";
+  } else {
+    document.getElementById("header").innerHTML = "It appears you're not at Beaurepaire";
+    document.getElementById("direction-btn").style.display = "block";
+    document.getElementById("tour-btn").style.display = "block";
   }
 }
 
