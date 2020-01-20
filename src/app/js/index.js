ee9e9ff01f3877664cbe475cb1d024e79c3d5cdd
Dervillay <james.gascoigneburns7@gmail.com>
2020-01-20 20:10:10 +0000

    Ask for location information on mobile only

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,28 +1,16 @@
 function getLocation() {
-  console.log(isMobile())
-  if (isMobile() && navigator.geolocation) {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else if (!navigator.geolocation) {
+  } else {
     alert("Geolocation is not supported by this browser.");
   }
 }
 
 function showPosition(position) {
   alert("Latitude: " + position.coords.latitude + "    Longitude: " + position.coords.longitude);
-  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 || 
-      position.coords.longitude > -1.621184 || position.coords.longitude < -1.6256) {
+  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 || position.coords.longitude > -1.621184 || position.coords.longitude < -1.6256) {
     alert("you aint in Beaurepaire")
   }
-}
-
-// Return whether user is viewing site on mobile
-function isMobile() {
-  var width = Math.max(document.documentElement.clientWidth, window.innerWidth);
-
-  if (width < 600) {
-    return true;
-  }
-  return false;
 }
 
 // 54.789525, -1.623473
