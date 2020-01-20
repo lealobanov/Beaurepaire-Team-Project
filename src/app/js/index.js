73df76d1e6a7f0e147e181b6fc8be4c0bef2892a
Dervillay <james.gascoigneburns7@gmail.com>
2020-01-20 20:14:55 +0000

    Ask for location info on mobile only

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,6 +1,5 @@
 function getLocation() {
-  console.log(isMobile())
-  if (isMobile() && navigator.geolocation) {
+  if (isMobile && navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else if (!navigator.geolocation) {
     alert("Geolocation is not supported by this browser.");
@@ -9,7 +8,7 @@ function getLocation() {
 
 function showPosition(position) {
   alert("Latitude: " + position.coords.latitude + "    Longitude: " + position.coords.longitude);
-  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 || 
+  if (position.coords.latitude > 54.790737 || position.coords.latitude < 54.788654 ||
       position.coords.longitude > -1.621184 || position.coords.longitude < -1.6256) {
     alert("you aint in Beaurepaire")
   }
