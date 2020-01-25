3ec971380010589e3979e9a3dd5bca1ec4686ef3
ksqk34 <thomas.roberts3@durham.ac.uk>
2020-01-25 21:42:05 +0000

    Implemented Beaurapaire model with temp coords

--- src/app/js/interactive_map_api.js
+++ src/app/js/interactive_map_api.js
@@ -56,12 +56,14 @@ async function Display(id_zone) {
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
+        Deselect_Zone();
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            Deselect_Zone();
         }
     }
 }
