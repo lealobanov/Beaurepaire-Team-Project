45cb4a421943a293f3dbe0eea1379d3dd057efd0
lealobanov <44328396+lealobanov@users.noreply.github.com>
2020-01-28 17:30:10 +0000

    Retrieve DB records

--- src/app/public/js/interactive_map_api.js
+++ src/app/public/js/interactive_map_api.js
@@ -80,33 +80,58 @@ async function Display(id_zone) {
 }
 // end of code inserted by Alex
 
-
-/**
- * This will be called upon loading.
- * To be filled out by Back-End crew.
- */
-async function Get_All_Zone_Data() {
-    let data = {};
-
-    // GET data from backend
-
-    return data;
+//Retrieve all data by individual ID
+async function Get_Feature_Data(id) {
+    let response = await fetch(`/features/retrieverecords/${id}`);
+    let body = await response.text();
+    let record = JSON.parse(body);
+    //console.log(record)
+    return record;
 }
 
-/**
- * This will be called upon loading.
- * To be filled out by Back-End crew.
- */
-async function Get_Coordinate_Data() {
-    let coordinates = {};
-
-    // GET coordinates from backend
-
-    return coordinates;
+//Retrieve all data
+async function GetAllData(){
+    let response = await fetch('/features/retrieverecords');
+    let body = await response.text();
+    let records = JSON.parse(body);
+    //console.log(records);
+    return records 
 }
 
+//Retrieve only modeling-specific data (feature id, coordinates 1, coordinates 2, rotation)
+async function Get_Modeling_Data() {
+    let data = await GetAllData();
+    modeling_data = [];
+    for (var i = 0; i < data.length; i++) { 
+        _id = data[i]._id;
+        // Probably need to parse these further into individual x1,y1,z1 and x2,y2,z2
+        coordinates_1 =  data[i].coordinates_1;
+        coordinates_2 =  data[i].coordinates_2;
+        rotation = data[i].rotation;
+        entry = {"_id": _id, "coordinates_1": coordinates_1, "coordinates_2": coordinates_2, "rotation": rotation}
+        modeling_data.push(entry);
+    }
+    //console.log(modeling_data);
+    return modeling_data
+}
+//Get_Modeling_Data();
 
-
+//Retrieve modal-specific data (feature id, modal title, modal content)
+async function Get_Modal_Data() {
+    let data = await GetAllData();
+    modal_data = [];
+    for (var i = 0; i < data.length; i++) { 
+        _id = data[i]._id;
+        //In the future images and other media will be served here
+        feature_title =  data[i].feature_title;
+        feature_content =  data[i].feature_content;
+        entry = {"_id": _id, "feature_title": feature_title, "feature_content": feature_content}
+        modal_data.push(entry);
+    }
+    //console.log(modal_data);
+    return modal_data
+}
+//Get_Modal_Data();
 
 /**
  * Callable function (If you need it):
