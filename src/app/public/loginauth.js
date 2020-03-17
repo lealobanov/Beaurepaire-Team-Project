8f36224911ba57040d1f67c774420f24bcba72f3
lealobanov <44328396+lealobanov@users.noreply.github.com>
2020-03-17 21:35:32 +0100

    Moved changes from master to develop

--- src/app/public/loginauth.js
+++ src/app/public/loginauth.js
@@ -10,34 +10,33 @@ var firebaseConfig = {
   measurementId: "G-VLTKYP402W"
 };
 // Initialize Firebase
-//firebase.initializeApp(firebaseConfig);
-
-
+firebase.initializeApp(firebaseConfig);
 
 // Defining Firebase authentication elements
 const txtEmail = document.getElementById('username_login');
 const txtPassword = document.getElementById('password_login');
 const btnLogin = document.getElementById('btnLogin');
-const btnLogout = document.getElementById('btnLogout');
+const btnLogoutTest = document.getElementById('btnLogoutTest');
+const btnLogoutHBS = document.getElementById('btnLogoutHBS');
 
 // Authenticate user login
 btnLogin.addEventListener('click', e => {
   const email = txtEmail.value;
-  console.log(email);
   const password = txtPassword.value;
-  console.log(password);
-  console.log("got click");
-  //Force to client portal w/out auth 
-  window.location = '/features/list';
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(val) {
+      //Success - redirect to admin page
+      window.location = './features/list';
+    })
+    
+    .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(errorMessage);
-    console.log("Login attempt failed.");
+    console.log(errorMessage)
+    console.log("Login attempt failed.")
     document.getElementById("errMsg").style.visibility = "visible";
-  });
-  
+  }); 
 
 });
 
@@ -45,21 +44,19 @@ btnLogin.addEventListener('click', e => {
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser){
     console.log("Logged in.");
-    //btnLogout.classList.remove('d-none');
-    //btnMyPosts.classList.remove('d-none');
-    //btnLoginheader.classList.add('d-none');
-    //btnRegisterheader.classList.add('d-none');
-    window.location = '/features/list';
   } else {
     console.log('Not logged in.');
-    //btnLogout.classList.add('d-none');
-    
   }
+});
+
+
 // Logout current user
-btnLogout.addEventListener('click', e => {
-  console.log("got logout click");
+
+btnLogoutHBS.addEventListener('click', e => {
   firebase.auth().signOut();
   window.location = '/backendlogin.html';
 });
 
-});
+
+
+
